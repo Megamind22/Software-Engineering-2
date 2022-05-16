3a7b83e50f1639661632a779022dc505ac59eaa1
MichaelMmdohAtef <77991985+MichaelMmdohAtef@users.noreply.github.com>
2022-05-16 20:43:19 +0200

    Add files via upload

--- src/test/Admin.java
+++ src/test/Admin.java
@@ -3,51 +3,51 @@ package test;
 import Database.AdminSQl;
 import java.io.*;
 
-public class Admin  {
+public class Admin extends AdminSQl  {
 
     public Admin() {
 
     }
 
     public void addStudentWithInitialCourses(Student student, String courseId) {
-        AdminSQl.addStudentData(student);
-        AdminSQl.addCoursesToStudent(student.getId(), courseId);
+        addStudentData(student);
+        addCoursesToStudent(student.getId(), courseId);
     }/*End_Of_addStudent*/
 
 
     public void addInstructorWithInitialCourse(Instructor instructor, String courseId) {
-        AdminSQl.addInstructor(instructor,courseId);
-        AdminSQl.addCoursesToInstructor(instructor.getId(), courseId);
+        addInstructor(instructor,courseId);
+        addCoursesToInstructor(instructor.getId(), courseId);
 
     }/*End_Of_addInstructor*/
 
     public void deleteStudent(int StudentId) {
-        AdminSQl.deleteStudent(StudentId);
+        deleteStudent(StudentId);
     }/*End_Of_deleteStudent*/
 
     public void updateStudent(String ID, String NewName)  {
 
-    AdminSQl.updateStudent(ID , NewName);
+    updateStudent(ID , NewName);
     }/*End_Of_updateStudent*/
     
      public void updateInstructor(String ID, String NewName)  {
 
-    AdminSQl.updateInstructor(ID , NewName);
+      updateInstructor(ID , NewName);
     }/*End_Of_updateStudent*/
 
 
 
     public void deleteInstructort(int instructorId) {
-        AdminSQl.deleteInstructor(instructorId);
+        deleteInstructor(instructorId);
     }/*End_Of_deleteInstructort*/
 
     public String shwoAllInstractor() {
-        return AdminSQl.shwoAllInstractors();
+        return shwoAllInstractors();
     }/*End_Of_shwoAllInstractor*/
 
     public String shwoAllStudent() {
 
-        return AdminSQl.shwoAllStudents();
+        return shwoAllStudents();
     }/*End_Of_shwoAllStudent*/
 
 
